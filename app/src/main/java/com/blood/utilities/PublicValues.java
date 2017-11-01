27feb1b9dca9791cea21231bcf27a9954d1ecfb1
joby <jobykorahgeorge@gmai.com>
2017-11-01 17:25:09 +0530

    commit

--- app/src/main/java/com/blood/utilities/PublicValues.java
+++ app/src/main/java/com/blood/utilities/PublicValues.java
@@ -11,6 +11,7 @@ public class PublicValues {
     public static final String DATE_OF_BIRTH = "dob";
     public static final String EMAIL_ID = "email";
     public static final String GENDER = "gender";
+    public static final String MOBILE_NUMBER_VERIFIED = "verified";
 
 
 
